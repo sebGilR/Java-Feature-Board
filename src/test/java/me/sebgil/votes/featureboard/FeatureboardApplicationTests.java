5579280bb03392734de92be7cc3db561ded86fbd
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-10-06 10:49:13 -0500

    Initialize Java Spring project

---
+++ src/test/java/me/sebgil/votes/featureboard/FeatureboardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sebgil.votes.featureboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
