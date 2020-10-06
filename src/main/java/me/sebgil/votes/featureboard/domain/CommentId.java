3fc0ef8bfc9ae505fb58588bd812e3d9e900e589
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-10-06 12:53:13 -0500

    Create joint tables fo comment and vote models

---
+++ src/main/java/me/sebgil/votes/featureboard/domain/CommentId.java
@@ -0,0 +1,33 @@
+package me.sebgil.votes.featureboard.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class CommentId implements Serializable {
+  private static final long serialVersionUID = -5085212086761283169L;
+  private User user;
+  private Feature feature;
+
+  @ManyToOne
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  @ManyToOne
+  public Feature getFeature() {
+    return feature;
+  }
+
+  public void setFeature(Feature feature) {
+    this.feature = feature;
+  }
+
+  
+}
---
+++ src/main/java/me/sebgil/votes/featureboard/domain/VoteId.java
@@ -0,0 +1,31 @@
+package me.sebgil.votes.featureboard.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class VoteId implements Serializable {
+  private static final long serialVersionUID = -3858448042946493295L;
+  private User user;
+  private Feature feature;
+
+  @ManyToOne
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  @ManyToOne
+  public Feature getFeature() {
+    return feature;
+  }
+
+  public void setFeature(Feature feature) {
+    this.feature = feature;
+  }
+}
